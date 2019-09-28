f7691f8d171e8c80d71daddb844cee05bd13e1ec
Tom Newton <tom.newton@shopify.com>
2019-09-28 10:41:39 -0400

    rejig env vars

--- services/default/src/server/server.ts
+++ services/default/src/server/server.ts
@@ -2,6 +2,15 @@ import {app} from './app';
 
 const PORT = process.env.PORT || 8080;
 
+if (
+  process.env.GOOGLE_CLOUD_PROJECT == 'indigo-griffin-254218' &&
+  process.env.NODE_ENV == 'production'
+) {
+}
+
+process.env.LOCATION = 'us-central1';
+process.env.DEFAULT_QUEUE = 'default';
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
