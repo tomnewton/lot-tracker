b965afe456e089fdf1fdf5392356436fab390edc
Tom Newton <tom.newton@shopify.com>
2019-10-03 20:12:46 -0400

    tweaks to allow for a subdir to contain a react client for the app

--- services/default/src/server/server.ts
+++ services/default/src/server/server.ts
@@ -27,6 +27,7 @@ async function start() {
   const app = await startApp();
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
+    console.log(`NODE_ENV == ${process.env.NODE_ENV}`);
   });
 }
 
